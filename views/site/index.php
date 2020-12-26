829dfaac52688cbb10dcc1cead2270463ff948b0
wadim <artemon271@yandex.ru>
2020-12-26 19:19:22 +0500

    В основном работа с Pjax

--- views/site/index.php
+++ views/site/index.php
@@ -2,36 +2,98 @@
 
 /* @var $this yii\web\View */
 
-use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use yii\helpers\Url;
 use yii\widgets\ListView;
 use yii\widgets\Pjax;
+use yii\helpers\VarDumper;
 
 $this->title = 'Список резюме';
-$script = <<< JS
-$(document).ready(function() {
-    $('#salary').change(function(evt) {
-        // console.log(evt.target);  + evt.target.value
-        $.pjax({
-            url: 'site/salary/' + evt.target.value,
-            success: function(res){
-                console.log($('#p0'));
-                $('#p0').replaceWith(res);
-                // window.location = location.href
-            },
-            error: function(){
-                console.log('Error!');
-            },
-            // container: '#p0',
-        },).reload({container: '#p0'});
-    });
-});
-JS;
-$this->registerJs($script);
+
 ?>
 
-<?php Pjax::begin(); ?>
+<?php 
+$script = <<< JS
+$(document).ready(function() {
+    // setting a default timeout for pjax
+    $.pjax.defaults.timeout = 1200;
+
+    $('#spec-search').nSelect({
+        firstTitle: 'Выберите профессию',
+        afterChange: function(el) {
+            $(this)[0].firstTitle = el[0].innerText;
+
+        }
+    });
+
+    $('#city-search').nSelect({
+        firstTitle: 'Выберите город',
+        afterChange: function(el) {
+            $(this)[0].firstTitle = el[0].innerText;
+
+        }
+    })
+
+    $('#salary').change(function(evt) {
+        const url = new URL(window.location);  // == window.location.href
+        url.searchParams.set('desired_salary', evt.target.value); 
+        history.pushState(null, null, url);    // == url.href
+        $.pjax({
+            url: url,
+            container: '#p0'
+        });
+    })
+    $('.field-spec-search>.nselect>.nselect__inner>ul>li>span').click(function(evt) {
+        const url = new URL(window.location);  // == window.location.href
+        url.searchParams.set('specialization', evt.target.parentElement.dataset.val); 
+        history.pushState(null, null, url);    // == url.href
+        $.pjax({
+            url: url,
+            container: '#p0'
+        });
+    })
+
+    $('.field-city-search>.nselect>.nselect__inner>ul>li>span').click(function(evt) {
+        const url = new URL(window.location);  // == window.location.href
+        url.searchParams.set('city', evt.target.parentElement.dataset.val); 
+        history.pushState(null, null, url);    // == url.href
+        $.pjax({
+            url: url,
+            container: '#p0'
+        });
+    })
+    $('#agefrom').change(function(evt) {
+        const url = new URL(window.location);  // == window.location.href
+        url.searchParams.set('agefrom', evt.target.value); 
+        history.pushState(null, null, url);    // == url.href
+        $.pjax({
+            url: url,
+            container: '#p0'
+        });
+    })
+    $('#ageto').change(function(evt) {
+        const url = new URL(window.location);  // == window.location.href
+        url.searchParams.set('ageto', evt.target.value); 
+        history.pushState(null, null, url);    // == url.href
+        $.pjax({
+            url: url,
+            container: '#p0'
+        });
+    })
+    $('input[type="radio"]').click(function(evt) {
+        const url = new URL(window.location);  // == window.location.href
+        url.searchParams.set('experience', $('input[type="radio"]:checked').val()); 
+        history.pushState(null, null, url);    // == url.href
+        $.pjax({
+            url: url,
+            container: '#p0'
+        });
+    })
+});
+
+JS;
+$this->registerJs($script);
+ ?>
 <div class="header-search">
     <div class="container">
         <div class="header-search__wrap">
@@ -62,43 +124,33 @@ $this->registerJs($script);
                         </div>
                         <div class="vakancy-page-wrap show">
                             <button class="vakancy-page-btn vakancy-btn dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="filter">Фильтровать</span>
+                                <span class="filter">Сортировать</span>
                                 <i class="fas fa-angle-down arrowDown"></i>
                             </button>
                             <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                <?= $listResume->sort->link(
-                                    'updated_at',
-                                    [
-                                        'label' => 'По новизне',
-                                        'class' => 'dropdown-item',
-                                        'default' => SORT_ASC,
-                                    ]
-                                ) ?>
-                                <?= $listResume->sort->link(
-                                    'desired_salary',
-                                    [
-                                        'label' => 'По зарплате',
-                                        'class' => 'dropdown-item',
-                                        //  'default' => SORT_DESC,
-                                    ]
-                                ) ?>
+
+                           <!-- sort links -->
+                                
                             </div>
                         </div>
                     </div>
                 </div>
+                <?php Pjax::begin([
+                    'timeout' => 100000, 
+                    'enablePushState' => true, 
+                    'formSelector' => '#searcher',
+                    'linkSelector' => '.run-pjax',
+                ]); ?>
 
                 <?= ListView::widget([
                     'dataProvider' => $listResume,
+
                     'itemView' => function ($model, $key, $index, $widget) {
                         return $this->render('_allresume', ['model' => $model]);
                     },
                     'layout' => "{items}\n{pager}",
                     'summary' => "Найдено {totalCount} резюме",
 
-                    // 'options' => [
-                    //     'tag' => 'div',
-                    //     'class' => 'vakancy-page-block company-list-search__block resume-list__block p-rel mb16'
-                    // ],
                     'itemOptions' => [
                         'tag' => 'div',
                         'class' => 'vakancy-page-block company-list-search__block resume-list__block p-rel mb16'
@@ -110,7 +162,7 @@ $this->registerJs($script);
                     ],
 
                 ]); ?>
-
+                <?php Pjax::end(); ?>
 
                 <ul class="dor-pagination mb128">
                     <li class="page-link-prev"><a href="#"><img class="mr8" src="/images/mini-left-arrow.svg" alt="arrow"> Назад</a>
@@ -132,120 +184,22 @@ $this->registerJs($script);
                 </div>
                 <div class="signin-modal__switch-btns-wrap resume-list__switch-btns-wrap mb16">
                     <!-- <a href="#" class="signin-modal__switch-btn ">Мужчины</a> -->
-                    <?= Html::a("Все", ['site/index'], ['class' => 'signin-modal__switch-btn active']) ?>
-                    <?= Html::a("Мужчины", ['site/man'], ['class' => 'signin-modal__switch-btn']) ?>
-                    <?= Html::a("Женщины", ['site/woman'], ['class' => 'signin-modal__switch-btn']) ?>
+                    
+                    <?= Html::a("Все", 
+                        Url::to(['site/index']), 
+                        ['class' => 'signin-modal__switch-btn active run-pjax', 'data-pjax' => 1]
+                    ) ?>
+                    <?= Html::a("Мужчины", Url::to(['/', 'gender' => 1]), ['class' => 'signin-modal__switch-btn data run-pjax', 'data-pjax' => 1]) ?>
+                    <?= Html::a("Женщины", Url::to(['/', 'gender' => 2]), ['class' => 'signin-modal__switch-btn data run-pjax', 'data-pjax' => 1]) ?>
                     <!-- <a href="#" class="signin-modal__switch-btn ">Женщины</a> -->
                 </div>
-                <div class="vakancy-page-filter-block__row mb24">
-                    <div class="paragraph cadet-blue">Город</div>
-                    <div class="citizenship-select">
-                        <?php if ($cities !== NULL) { ?>
-
-                            <!-- <select class="nselect-1"> -->
-                            <div class="dropdown w-100 nselect ns-sys nsOrange dropdownCity">
-                                <button class="dropdown-toggle w-100 nselect__head" type="button" id="dropdownCityButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Выберите город
-                                </button>
-                                <div class="dropdown-menu" aria-labelledby="dropdownCityButton">
-                                    <?php foreach ($cities as $city) { ?>
-                                        <?= Html::a($city->city, ['site/city/' . $city->city], ['class' => 'dropdown-item']) ?>
-                                    <?php } ?>
-                                    <!-- <a class="dropdown-item" href="#">Action</a>
-                                        <a class="dropdown-item" href="#">Another action</a>
-                                        <a class="dropdown-item" href="#">Something else here</a> -->
-                                </div>
-                            </div>
-
-
-                            <!-- </select> -->
-                        <?php } ?>
-                    </div>
-                </div>
-                <div class="vakancy-page-filter-block__row mb24">
-                    <div class="paragraph cadet-blue">Зарплата</div>
-                    <div class="p-rel">
-                        <!-- <input placeholder="Любая" type="text" class="dor-input w100" id="salary"> -->
-                        <?= Html::input('text', 'desired_salary', ['id' => 'salary', 'class' => 'dor-input w100', 'placeholder']) ?>
-                        <img class="rub-icon" src="/images/rub-icon.svg" alt="rub-icon">
-                    </div>
-                </div>
-                <div class="vakancy-page-filter-block__row mb24">
-                    <div class="paragraph cadet-blue">Специализация</div>
-                    <div class="citizenship-select">
-                        <select class="nselect-1" data-title="Любая">
-                            <option value="01">Фронтенд</option>
-                            <option value="02">Бекенд</option>
-                            <option value="03">Дизайн</option>
-                            <option value="04">Тестировщик</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="vakancy-page-filter-block__row mb24">
-                    <div class="paragraph cadet-blue">Возраст</div>
-                    <div class="d-flex">
-                        <input placeholder="От" type="text" class="dor-input w100">
-                        <input placeholder="До" type="text" class="dor-input w100">
-                    </div>
-                </div>
-                <div class="vakancy-page-filter-block__row mb24">
-                    <div class="paragraph cadet-blue">Опыт работы</div>
-                    <div class="profile-info">
-                        <div class="form-check d-flex">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1"></label>
-                            <label for="exampleCheck1" class="profile-info__check-text">Без опыта</label>
-                        </div>
-                        <div class="form-check d-flex">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck2">
-                            <label class="form-check-label" for="exampleCheck2"></label>
-                            <label for="exampleCheck2" class="profile-info__check-text">От 1 года до 3
-                                лет</label>
-                        </div>
-                        <div class="form-check d-flex">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck3">
-                            <label class="form-check-label" for="exampleCheck3"></label>
-                            <label for="exampleCheck3" class="profile-info__check-text">От 3 лет до 6
-                                лет</label>
-                        </div>
-                        <div class="form-check d-flex">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck4">
-                            <label class="form-check-label" for="exampleCheck4"></label>
-                            <label for="exampleCheck4" class="profile-info__check-text">Более 6 лет</label>
-                        </div>
-                    </div>
-                </div>
-                <div class="vakancy-page-filter-block__row mb24">
-                    <div class="paragraph cadet-blue">Тип занятости</div>
-                    <div class="profile-info">
-                        <div class="form-check d-flex">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck5">
-                            <label class="form-check-label" for="exampleCheck5"></label>
-                            <label for="exampleCheck5" class="profile-info__check-text">Полная занятость</label>
-                        </div>
-                        <div class="form-check d-flex">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck6">
-                            <label class="form-check-label" for="exampleCheck6"></label>
-                            <label for="exampleCheck6" class="profile-info__check-text">Частичная
-                                занятость</label>
-                        </div>
-                        <div class="form-check d-flex">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck7">
-                            <label class="form-check-label" for="exampleCheck7"></label>
-                            <label for="exampleCheck7" class="profile-info__check-text">Проектная работа</label>
-                        </div>
-                        <div class="form-check d-flex">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck8">
-                            <label class="form-check-label" for="exampleCheck8"></label>
-                            <label for="exampleCheck8" class="profile-info__check-text">Стажировка</label>
-                        </div>
-                        <div class="form-check d-flex">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck9">
-                            <label class="form-check-label" for="exampleCheck9"></label>
-                            <label for="exampleCheck9" class="profile-info__check-text">Волонтёрство</label>
-                        </div>
-                    </div>
-                </div>
+                
+                <?= $this->render('_search', ['model' => $searchModel]) ?>
+                <!-- filters -->
+                
+                
+                
+                
                 <div class="vakancy-page-filter-block__row mb24">
                     <div class="paragraph cadet-blue">График работы</div>
                     <div class="profile-info">
@@ -282,8 +236,9 @@ $this->registerJs($script);
                         вакансии</a>
                     <a href="#">Сбросить все</a>
                 </div>
+                
+                <!-- filters -->
             </div>
         </div>
     </div>
 </div>
-<?php Pjax::end(); ?>
