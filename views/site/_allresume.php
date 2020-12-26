34d0358c90745f40d4e88ab1126025f0cb153c07
wadim <artemon271@yandex.ru>
2020-12-26 19:16:23 +0500

    Использовал модели перечислений в представлении

--- views/site/_allresume.php
+++ views/site/_allresume.php
@@ -1,5 +1,9 @@
 <?php 
 
+use app\models\enums\Specialist;
+use app\models\enums\Cities;
+use app\models\enums\Gender;
+use app\models\enums\Monts;
 
 ?>
 
@@ -24,31 +28,43 @@
         </div>
     <?php } ?>
 
-    <h3 class="mini-title mobile-off"><?= $model->specialization ?></h3>
+    <h3 class="mini-title mobile-off"><?= Specialist::getLabel($model->specialization) ?></h3>
     <div class="d-flex align-items-center flex-wrap mb8 ">
         <span class="mr16 paragraph"><?= $model->desired_salary ?> ₽</span>
-        <span class="mr16 paragraph">Опыт работы <?= $model->exp[0]->year_end_work - $model->exp[0]->year . ' ' . $model->num2word($model->exp[0]->year_end_work - $model->exp[0]->year, ['год', 'года', 'лет']) ?></span>
+        <?php if($model->exp[0]){ ?> 
+        <span class="mr16 paragraph">
+            Опыт работы 
+            <?= $model->exp[0]->year_end_work - $model->exp[0]->year . ' ' . 
+                $model->num2word($model->exp[0]->year_end_work - $model->exp[0]->year, 
+                ['год', 'года', 'лет']) ?>
+        </span>
+        <?php } ?>
         <span class="mr16 paragraph">
             <?php 
-                $age = Yii::$app->formatter->asDate('now', 'php:Y') - Yii::$app->formatter->asDate($model->birthday, 'php:Y');
+                $age = Yii::$app->formatter->asDate('now', 'php:Y') - 
+                       Yii::$app->formatter->asDate($model->birthday, 'php:Y');
                 echo $age . $model->num2word($age, [' год', ' года', ' лет']);
              ?>
         </span>
-        <span class="mr16 paragraph"><?= $model->city ?></span>
+        <span class="mr16 paragraph"><?= Cities::getLabel($model->city) ?></span>
+        <span class="mr16 paragraph"><?= Gender::getLabel($model->gender) ?></span>
     </div>
-    <p class="paragraph tbold mobile-off">Последнее место работы</p>
+    <?php if($model->exp[0]){ ?>
+        <p class="paragraph tbold mobile-off">Последнее место работы</p>
+        <div class="company-list-search__block-middle">
+            <h3 class="mini-title desktop-off"><?= $model->exp[0]->organization ?></h3>
+            <p class="paragraph mb16 mobile-mb32">
+                <?= Specialist::getLabel($model->exp[0]->exp_spec) ?>
+            </p>
+            <p class="paragraph mb16 mobile-mb32">
+                <?= Monts::getLabel($model->exp[0]->month) . ' ' . $model->exp[0]->year ?> — 
+                <?php if($model->exp[0]->until_now_work === true) {
+                    echo 'по настоящее время';
+                } else {
+                    echo Monts::getLabel($model->exp[0]->month_end_work) . ' ' . $model->exp[0]->year_end_work;
+                } ?>
+            </p>
+        </div>
+    <?php } ?>
 </div>
-<div class="company-list-search__block-middle">
-    <h3 class="mini-title desktop-off"><?= $model->exp[0]->organization ?></h3>
-    <p class="paragraph mb16 mobile-mb32">
-        <?= $model->exp[0]->exp_spec ?>
-    </p>
-    <p class="paragraph mb16 mobile-mb32">
-        <?= $model->exp[0]->month . ' ' . $model->exp[0]->year ?> — 
-        <?php if($model->exp[0]->until_now_work !== NULL) {
-            echo 'по настоящее время';
-        } else {
-            echo $model->exp[0]->month_end_work . ' ' . $model->exp[0]->year_end_work;
-        } ?>
-    </p>
-</div>
+
